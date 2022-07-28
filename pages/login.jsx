283882dde819265375cc0aa2caf0963c001cebd5
madhuridigicita <94893018+madhuridigicita@users.noreply.github.com>
2022-07-28 18:54:32 +0530

    chages

--- pages/login.jsx
+++ pages/login.jsx
@@ -23,7 +23,7 @@ const SignUp = (props) => {
         {/* <Header /> */}
         <div className='container mt-5'>
             <div className='text-center' style={isMobile ? { marginTop: '100px', marginBottom: '135px' } : {}}>
-                <text className='fs-16 ff-roboto'> Welcome to</text>
+                <text className='fs-16 ff-roboto'> Welcome to </text>
                 <h2 className='ff-roboto fs-30 fw-bold'>{getPath === "create" ? "THINKLY" : getPath === "stars" && "GET STARS"}</h2> <br />
                 <p className='font-weight-bold fs-20 ff-roboto'>Create New Account</p>
                 <button type='button' className='button-social-link mt-2 height-button fs-18 bg-white fc-black ff-roboto border-radius-4' onClick={() => handleGmailSignIn()}>
