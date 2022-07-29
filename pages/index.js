3a85e50ad377175fb35795e93a932b2c3c75a32c
madhuridigicita <94893018+madhuridigicita@users.noreply.github.com>
2022-07-29 14:23:42 +0530

    changes

--- pages/index.js
+++ pages/index.js
@@ -6,25 +6,6 @@ import Head from 'next/head';
 const Home = () => (
   <div className="home">
     <h1>What Can I Deploy to Static Apps?</h1>
-    
-    {/* <div className="card-grid">
-      {projects.map((project) => {
-        const Icon = projectIcons[project.id];
-        return (<div key={project.id}>
-          <Head>
-            <title> {project.name} </title>
-            <meta name='og:title' content={"og title: " + project.name} />
-            <meta name="og:description" content={"og description: " + project.slug} />
-          </Head>
-          <SmallCard
-            key={project.id}
-            Icon={Icon}
-            title={project.name}
-            slug={project.slug}
-          />
-        </div>);
-      })}
-    </div> */}
   </div>
 );
 
