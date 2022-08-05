7e97d626fcc3b22ff1d76392d43dd1753bb92731
madhuridigicita <94893018+madhuridigicita@users.noreply.github.com>
2022-08-05 14:55:16 +0530

    authenticate is user

--- pages/user/authenticateIsUser.js
+++ pages/user/authenticateIsUser.js
@@ -1,11 +1,11 @@
 // user authentication after userAuth.js called
 import firebase from 'firebase/compat/app'
 
-const AuthenticateIsUser = async (email, url) => {
+const AuthenticateIsUser = async (email) => {
     console.log("inside authenticateIsUser function1");
-    if (firebase.auth().isSignInWithEmailLink(url)) {
+    if (firebase.auth().isSignInWithEmailLink(window.location.href)) {
         console.log("inside if isSignInWithEmailLink condition -> authenticateIsUser function1");
-        await firebase.auth().signInWithEmailLink(email, url)
+        await firebase.auth().signInWithEmailLink(email, window.location.href)
             .then((result) => {
                 console.log("inside .then -> authenticateIsUser function1", result);
                 localStorage.setItem('signInStatus', 'Success');
