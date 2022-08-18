60187b87faaeb4053ac7aafa878eb8b08e65a503
preeti.joshi@digicita.com <preeti.joshi@digicita.com>
2022-08-18 15:41:29 +0530

    hello.js modified

--- static/hello.js
+++ static/hello.js
@@ -1,15 +1,18 @@
 import Head from 'next/head';
-
+import 'next/script';
 export default function Hello() {
     return (
         <div>
             <Head>
-                <script type="text/javascript" src="static/libs/jquery.min.js"></script>
+              
                 {/* <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/malihu-custom-scrollbar-plugin@3.1.5/jquery.mCustomScrollbar.concat.min.js"></script> */}
-                <script type="text/javascript" src="static/libs/bootstrap.min.js"></script>
+                <script type="text/javascript" src="./components/common/header.jsx"></script>
                 {/* <script type="text/javascript" src="static/libs/owl.carousel.min.js"></script>
                 <script type="text/javascript" src="static/scripts/chatHead.js"></script>
                 <script type="text/javascript" src="static/libs/jquery.magnific-popup.js"></script> */}
+                <script>
+                    alert('test');
+                </script>
             </Head>
             <h1>A page of my website.</h1>
         </div>
