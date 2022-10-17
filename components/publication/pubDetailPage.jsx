28421a1ffa5926fd49205c10ab2b59e6af051a36
madhuridigicita <94893018+madhuridigicita@users.noreply.github.com>
2022-10-17 12:09:37 +0530

    changes

--- components/publication/pubDetailPage.jsx
+++ components/publication/pubDetailPage.jsx
@@ -4,7 +4,9 @@ import Axios from "axios";
 import { useRouter } from 'next/router'
 import Image from 'next/image';
 import Head from 'next/head';
-import { CircularProgress } from '@material-ui/core';
+import { Avatar, Card, CardMedia, CardHeader, CardContent, CircularProgress } from '@material-ui/core'
+import Carousel from "react-multi-carousel"
+import "react-multi-carousel/lib/styles.css"
 import Header from '../common/header';
 import Footer from '../common/footer';
 import { PublicationProfileEvent } from '../../config/facebookPixelEvent';
@@ -22,6 +24,17 @@ const PublicationDetailPage = (props) => {
     const [AuthorList, setAuthorList] = useState()
     const [UserStarBalance, setUserStarBalance] = useState()
     const [PubSection, setPubSection] = useState()
+    const [getPublicationThinkly, setPublicationThinkly] = useState()  //thinklies list in this publication
+    const [startIndexValue, setstartIndexValue] = useState(0)  //star index of load ore thinkies in this publication
+    const [endIndexValue, setendIndexValue] = useState(9)  //end index of load ore thinkies in this publication
+    const [NoMoreData, setNoMoreData] = useState(false)  //if no more thinklies then hide MORE link 
+    const [getUserPublication, setUserPublication] = useState() // more publication by the author who has written this publilcation
+    const [getMorePubByInterest, setMorePubByInterest] = useState()  //more publication list by this publication's interest
+    // const [publicationPrice, setPublicationPrice] = useState() // store publication price
+    const [submitLoader, setSubmitLoader] = useState(false) //loader hide and show on payment btn
+
+    const [emailID, setemailID] = useState()
+    const [CoAuthorList, setCoAuthorList] = useState()
 
     useEffect(() => {
         if (process.env.NEXT_PUBLIC_GOOGLE_PIXEL_EVENT === 'YES') {
@@ -81,7 +94,7 @@ const PublicationDetailPage = (props) => {
         setAuthorList(arr)  //main author index switch last to start end here
         setUserStarBalance(response.userDetails.userBalance)  //userBalance stored in state
         fetchSectionDetail(data.publicationID, data.createdBy) //function call
-        // fetchPublicationThinklies(data.publicationID, data.createdBy)  //function call
+        fetchPublicationThinklies(data.publicationID, data.createdBy)  //function call
         // fetchMorePubByAuthor(data.publicationID, data.createdBy)   //function call
         // const interestArray = [];
         // data.interestData.filter((obj) => {
@@ -104,6 +117,212 @@ const PublicationDetailPage = (props) => {
                     setPubSection(response)
                 }
             })
+    }
+
+    // fetch more thinkly by publication start
+    const fetchMoreThinklyByPublication = () => {
+        scrollThinklies()
+    }
+
+    function scrollThinklies() {
+        setstartIndexValue(endIndexValue)
+        setendIndexValue(endIndexValue + 9)
+    }
+
+    // useEffect(() => { //don't delete uncomment when use fetchPublicationThinklies api
+    //     if (getPublicationID !== undefined && getAuthorID !== undefined) {
+    //         fetchPublicationThinklies(getPublicationID, getAuthorID)
+    //     }
+    // }, [startIndexValue, endIndexValue])
+
+    function fetchPublicationThinklies(p_id, authorId) {
+        var config = {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json",
+                "DeviceID": process.env.NEXT_PUBLIC_DEVICE_ID,
+                "UserID": authorId
+            },
+            data: {
+                "PublicationID": p_id,
+                "UserID": authorId,
+                "ThinklyID": 0,
+                "StartIndex": startIndexValue,
+                "EndIndex": endIndexValue
+            }
+        };
+        Axios(`${BASE_URL}Thinkly/GetPublicationThinklies`, config)
+            .then((res) => {
+                if (res.data.responseCode === '00') {
+                    const newData = res.data.responseData;
+                    if (getPublicationThinkly !== undefined) {
+                        setPublicationThinkly(getPublicationThinkly => [...getPublicationThinkly, ...newData])
+                    } else {
+                        setPublicationThinkly(newData)
+                    }
+                    if (newData.length < 9) {
+                        setNoMoreData(true)
+                    }
+                } else if (res.data.responseCode === '03') {
+                    setNoMoreData(true)
+                }
+            })
+            .catch((err) => {
+                console.log("GetPublicationThinklies error in catch", err);
+            });
+    }
+    // fetch more thinkly by publication end
+
+    const fetchMorePubByAuthor = (p_id, authorId) => {
+        var config = {
+            method: 'POST',
+            headers: {
+                "DeviceID": process.env.NEXT_PUBLIC_DEVICE_ID,
+                "UserID": authorId
+            },
+            data: {
+                "UserID": authorId,
+                "PublicationID": p_id,
+                "StartIndex": 0,
+                "EndIndex": 10
+            }
+        };
+        Axios(`${BASE_URL}User/GetUserPublications`, config)
+            .then((res) => {
+                if (res.data.responseCode === '00') {
+                    setUserPublication(res.data.responseData)
+                }
+            })
+            .catch((err) => {
+                console.log("fetch more publication by author @@@@@@@@", err);
+            });
+    }
+
+    const fetchYouMayLikePubs = (authorId, interestArray) => {
+        var config = {
+            method: 'POST',
+            headers: {
+                "DeviceID": process.env.NEXT_PUBLIC_DEVICE_ID,
+                "UserID": authorId
+            },
+            data: {
+                "UserID": authorId,
+                "StartIndex": 0,
+                "EndIndex": 10,
+                "PublicationID": 0,
+                "Interest": interestArray
+            }
+        };
+        Axios(`${BASE_URL}Publication/GetOtherPublicationsYouMayLike`, config)
+            .then((res) => {
+                if (res.data.responseCode === '00') {
+                    setMorePubByInterest(res.data.responseData)
+                }
+            })
+            .catch((err) => {
+                console.log("fetch more publication you may like@@@@@@@@", err);
+            });
+    }
+
+    const singleAuthor = (obj) => {
+        const imageUrl = obj.authorProfileImage.charAt(0) === '@' ? obj.authorProfileImage.substring(1) : obj.authorProfileImage
+        // onClick={() => handleUserProfile(obj.authorID)}
+        return (<Card className="t-in-p">
+            <div className='mb-3'>
+                <Avatar className='mx-auto ma-profile' src={imageUrl} alt="author profile" />
+            </div>
+            <div className='text-center mb-2'>
+                <p className='fs-26 fw-bold'>
+                    {obj.authorName.slice(0, 18) + (obj.authorName.length > 18 ? "..." : "")}
+                </p>
+            </div>
+            <p className='fs-18 text-center'>
+                {obj.authorProfileText.length > 200 ? <> {obj.authorProfileText.slice(0, 200)} <span className='fw-mid-bold pointer' onClick={() => handleUserProfile(obj.authorID)} >...read more</span> </> : obj.authorProfileText}
+            </p>
+        </Card>)
+    }
+
+    const handleUserProfile = (authorID) => {
+        var config = {
+            headers: {
+                "Content-Type": "application/json",
+                "DeviceID": process.env.NEXT_PUBLIC_DEVICE_ID,
+                "UserID": authorID
+            },
+        };
+        Axios.get(`${BASE_URL}User/GetUserProfileByID/${authorID}`, config)
+            .then((res) => {
+                // logEvent(analytics, 'PUB_ATHOUR_CLICK', { authorID: authorID })
+                if (res.data.responseCode === '00') {
+                    const response = res.data.responseData.profileDetails
+                    const url = response.profileUrl.charAt(0) === '@' ? response.profileUrl.substring(1) : response.profileUrl
+                    window.open(url, '_blank')
+                }
+            })
+            .catch((err) => {
+                console.log("GetUserProfileByID error in catch", err);
+            });
+    }
+
+    const onSubmit = (freePub) => {  //razor pay use for paid pub subscription
+        setSubmitLoader(true)
+        return new Promise(resolve => {
+            const price = freePub === 'Free' ? 0 : parseFloat(publicationPrice).toFixed(2); //if free then 0 price 
+            const refName = ParaByNameFromUrl('referrer')
+            const form = document.createElement('form');
+            form.method = 'post'
+            form.action = freePub === 'Free' ? process.env.NEXT_PUBLIC_PAYMENT_GATEWAY_RAZORPAY_FREE : process.env.NEXT_PUBLIC_PAYMENT_GATEWAY_RAZORPAY
+            const data = [
+                { name: 'qty', value: "1" }, //pass hardcoded 1 only
+                { name: 'receiver', value: getAuthorID },
+                { name: 'subscribedby', value: '0' },  //pass 0 cause not matter if logged in or not
+                { name: 'remarks', value: "subscribe to publication" },
+                { name: 'channel', value: "WEBAPP" },
+                { name: 'amount', value: price },
+                { name: 'producttype', value: "PUBLICATIONSUBSCRIPTION" },
+                { name: 'publicationid', value: getPublicationID },
+                { name: 'subscriptiontype', value: freePub === 'Free' ? "FREE" : "PAID" }, //free or paid
+                { name: 'publicationpenname', value: publicationPenName },
+                { name: 'emailid', value: freePub === 'Free' ? emailID : '' }, //for free only
+                { name: 'referrer', value: refName !== undefined ? refName : '' }
+            ]
+            for (let x = 0; x < data.length; x++) {
+                const hiddenField = document.createElement('input');
+                hiddenField.type = 'text';
+                hiddenField.name = data[x].name;
+                hiddenField.value = data[x].value;
+                hiddenField.style.display = 'none'
+                form.appendChild(hiddenField);
+            }
+            // logEvent(analytics, 'PUB_PAYMENT_GATEWAY', { payToPub: freePub === 'Free' ? '' : getPublicationID }) //google analytic log
+            document.body.appendChild(form);
+            if (process.env.NEXT_PUBLIC_GOOGLE_PIXEL_EVENT === 'YES') {
+                PublicationSubscribeEvent()  //google pixel event
+            }
+            form.submit();
+            resolve();
+        });
+    }
+
+    const subscribeHandle = (publicationPayType) => {
+        // logEvent(analytics, 'PUB_SUBSCIRBE_CLICK', { 'payType': publicationPayType })
+        if (publicationPayType === 'Free') {
+            $('#userContactInfo').modal('show')
+        } else {
+            document.getElementById("paidpublicationdiv").scrollIntoView({ behavior: 'smooth' });
+        }
+    }
+
+    const viewpub = (penName) => {
+        const name = penName.charAt(0) === '@' ? penName.substring(1) : penName;
+        // logEvent(analytics, 'PUB_AUTHOR_PUBLICATION_CLICK', { pubPenName: penName })  //google analytic log
+        var newWindow = window.open(`${name}`, '_blank')
+        newWindow.penName = name
+    }
+
+    const handlePostView = (oldUrl, postID) => {
+        // logEvent(analytics, 'PUB_POST_CLICK', { thinklyID: postID }) //google analytic log
+        window.open(oldUrl, '_blank')
     }
 
     return (<>
@@ -142,6 +361,22 @@ const PublicationDetailPage = (props) => {
                 </div>
                 <div className='vertical-line mt-5 mb-4'></div>
             </>}
+            {/* for free publication get user mail ID MODAL */}
+            <div id="userContactInfo" class="modal fade" role="dialog" data-backdrop="static" data-keyboard="false">
+                <div class="modal-dialog modal-dialog-centered">
+                    <div class="modal-content">
+                        <button type="button" class="close text-right pr-2" data-dismiss="modal" >&times;</button>
+                        <div class="modal-body px-5 pb-4 pt-1">
+                            <h5 className='text-center mb-4'>We need your emailID to send you a voucher code</h5>
+                            <input type='text' placeholder='Your email ID' value={emailID} onChange={(e) => setemailID(e.target.value)} style={{ fontSize: '20px', border: 'none', outline: 'none', width: '100%' }} />
+                            {<div id="infoPlease" className='error-msg'></div>}
+                            <div className='text-center'>
+                                <button type='submit' className='mt-3 pointer fw-mid border-radius-4 fc-white border-none height-button fs-18 w-50 primary-bg-color' onClick={() => onSubmit('Free')} >Continue</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
             {/* sections */}
             {PubSection !== undefined && PubSection.length > 0 && <>
                 {PubSection.map((obj, index) => {
@@ -180,6 +415,31 @@ const PublicationDetailPage = (props) => {
                     </>)
                 })}
             </>}
+            {/* thinklies in this publication */}
+            {getPublicationThinkly !== undefined && getPublicationThinkly.length > 0 && <>
+                <div className='row'>
+                    <div className='col-12 text-center fs-28 fw-bold mb-4'> Posts in this Publication </div>
+                    {getPublicationThinkly.map((obj, index) => {
+                        const image_url = obj.postData.postImages.length > 0 && obj.postData.postImages[0].charAt(0) === '@' ? obj.postData.postImages[0].substring(1) : obj.postData.postImages[0]
+                        return (<div className='col-4' key={index}>
+                            <Card className='t-in-p' onClick={() => handlePostView(obj.postData.postURL, obj.postData.postID)}>
+                                <div className='row d-flex'>
+                                    <div className='col-2 image-container'>
+                                        {obj.postData.postImages.length > 0 ? <Image src={image_url} className='image' layout="fill" />
+                                            : <Card className='publilcation-image' style={{ background: '#faa422' }}></Card>}
+                                    </div>
+                                    <div className='col-8 my-auto ml-3 fs-18'> {obj.postData.postTitle.slice(0, 44) + (obj.postData.postTitle.length > 44 ? "..." : "")} </div>
+                                </div>
+                            </Card>
+                        </div>)
+                    })}
+                </div>
+                {!NoMoreData && <div>
+                    <p className='mt-4 pointer fs-18 fw-bold' onClick={() => fetchMoreThinklyByPublication()}> <KeyboardArrowDown /> show more</p>
+                </div>}
+                <div className='vertical-line mt-5 mb-5'></div>
+            </>}
+
         </div> : <div className='grid place-items-center h-screen'>
             <CircularProgress aria-label="Loading..." />
         </div>}
